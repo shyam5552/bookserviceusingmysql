8b340a7ce797d3fd5e0233aec3173f41fe6fb28a
Ramakrishna_Doddi <ramakrishna+_doddi@epam.com>
2024-01-14 12:34:37 +0530

    refactoring

--- bookserviceusingmysql/src/main/java/com/example/bookserviceusingmysql/bookentity/Book.java
+++ bookserviceusingmysql/src/main/java/com/example/bookserviceusingmysql/bookentity/Book.java
@@ -1,8 +1,8 @@
 package com.example.bookserviceusingmysql.bookentity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Book {
--- bookserviceusingmysql/src/main/java/com/example/bookserviceusingmysql/security/SecurityConfigure.java
+++ bookserviceusingmysql/src/main/java/com/example/bookserviceusingmysql/security/SecurityConfigure.java
@@ -3,6 +3,7 @@ package com.example.bookserviceusingmysql.security;
 import com.example.bookserviceusingmysql.servicel.MyuserdetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyuserdetailsService myuserdetailsService;
